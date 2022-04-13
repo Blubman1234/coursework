ba46ca2b9b906f3564e5d6e0c61170d958031ac8
Thomas <thomas.gibbons5@gmail.com>
2022-04-13 15:39:52 +0100

    script will insert listener for CRT test if it cant insert one for SRT

--- test_script.js
+++ test_script.js
@@ -1,6 +1,11 @@
 /*test_script.js created:28/03/22*/
+try{
+	document.getElementById("start_SRT_button").addEventListener("click", srt_test);
+}
+catch(TypeError){
+	document.getElementById("start_CRT_button").addEventListener("click", srt_test);
+}
 
-document.getElementById("start_SRT_button").addEventListener("click", srt_test);
 
 /*this function take a time in milliseconds and will output the date time when that time has passed*/
 function get_time_from_now(milliseconds){
