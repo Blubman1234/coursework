ce278ecc84df841891284e52211b492a26409eb3
Thomas <thomas.gibbons5@gmail.com>
2022-04-25 23:14:01 +0100

    added crt warning message for when choice accuracy < 80%

--- test_result_script.js
+++ test_result_script.js
@@ -33,12 +33,32 @@ function display_crt_accuracy(currCrtResult){
 	const accuracy = currCrtResult.accuracy
 	document.getElementById("crt_accuracy").innerHTML = "Accuracy <br>%"+ accuracy;
 }
+
 function display_crt_results(){
 	const crtResults = get_results("crtResults");
 	display_attempt_number(crtResults);
 	currCrtResult = crtResults[crtResults.length - 1];
 	display_crt_time(currCrtResult);
 	display_crt_accuracy(currCrtResult);
+	//check if accuracy is less than 80%
+	if(currCrtResult.accuracy < 80){
+		//remove current result from localStorage
+		crtResults.pop();
+		let resultString = JSON.stringify(crtResults);
+		localStorage.setItem("crtResults",resultString);
+		//listen to see if user accepts current result
+		let warning_message = document.getElementById("warning_message");
+		warning_message.style.display = "block"
+		let accept_button = document.getElementById("accept_result_button")
+		accept_button.addEventListener("click",function reinsert_current_result(){
+			crtResults.push(currCrtResult);
+			let resultString = JSON.stringify(crtResults);
+			localStorage.setItem("crtResults",resultString);
+			//remove warning_message
+			warning_message.style.display = "none";
+		});
+	}
+
 }
 
 function main() {
